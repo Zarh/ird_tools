9610a207c689dc2a06c4fafab22f9ce1979c284b
Zarh <zarkyc@gmail.com>
2021-06-25 21:00:26 +0200

    fix endianness

--- ird_build.h
+++ ird_build.h
@@ -21,8 +21,11 @@ extern u8 verbose;
 
 #define print_verbose(...) 	if( verbose ) print_load(__VA_ARGS__)
 
-#define SUCCESS		1
-#define FAILED 		0
+#define SUCCESS 	1
+#define FAILED	 	0
+
+#define IS_BIG_ENDIAN 			(!*(unsigned char *)&(uint16_t){1})
+#define IS_LITTLE_ENDIAN		(*(unsigned char *)&(uint16_t){1})
 
 #define ENDIAN_SWAP_16(x)		(((x) & 0x00FF) << 8 | ((x) & 0xFF00) >> 8)
 #define ENDIAN_SWAP_32(x)		(((x) & 0x000000FF) << 24 | ((x) & 0x0000FF00) << 8 | \
@@ -31,11 +34,18 @@ extern u8 verbose;
 								 ((x) & 0x0000000000FF0000ULL) << 24 | ((x) & 0x00000000FF000000ULL) <<  8 | \
 								 ((x) & 0x000000FF00000000ULL) >>  8 | ((x) & 0x0000FF0000000000ULL) >> 24 | \
 								 ((x) & 0x00FF000000000000ULL) >> 40 | ((x) & 0xFF00000000000000ULL) >> 56 )
-//#define ENDIAN_SWAP(x)			(sizeof(x) == 2 ? ENDIAN_SWAP_16(x) : (sizeof(x) == 4 ? ENDIAN_SWAP_32(x) : ENDIAN_SWAP_64(x)))
-#define ENDIAN_SWAP(x)          x
+#define ENDIAN_SWAP(x)			(sizeof(x) == 2 ? ENDIAN_SWAP_16(x) : (sizeof(x) == 4 ? ENDIAN_SWAP_32(x) : ENDIAN_SWAP_64(x)))
+
+// note for me...
+// SWAP to/from BigEndian & LittleEndian
+// if the file is in LittleEndian (like IRD) use SWAP_LE
+// if it's BigEndian (like ISO) use SWAP_BE
+#define SWAP_BE(x)				(IS_BIG_ENDIAN    ? x : ENDIAN_SWAP(x))
+#define SWAP_LE(x)				(IS_LITTLE_ENDIAN ? x : ENDIAN_SWAP(x))
 
 #define FREE(x)					if(x!=NULL) {free(x);x=NULL;}
 #define FCLOSE(x) 				if(x!=NULL) {fclose(x);x=NULL;}
+
 #define FREE_IRD(x)				if(x!=NULL) {                                                                  \
 									int o;                                                                     \
 									FREE(x->GameName);                                                         \
